f81538fc14454e98929aab2ee960e7d65bfc09e1
shivani <shivanidiwakar2211@gmail.com>
2026-06-18 23:27:08 +0530

    fix api url

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,5 +1,6 @@
-export const USER_API_END_POINT = "http://localhost:4000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:4000/api/v1/job";
-export const APPLICATION_API_END_POINT =
-  "http://localhost:4000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:4000/api/v1/company";
+const BASE_URL = "https://hireai-backend-nyu7.onrender.com/api/v1";
+
+export const USER_API_END_POINT = `${BASE_URL}/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/job`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/company`;
